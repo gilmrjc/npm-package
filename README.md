6e42fca5dd36aac2213f879da867aa1cb7f7e60f
Gildardo Adrian Maravilla Jacome <gilmrjc@gmail.com>
2021-02-14 03:26:12 -0600

    feat: Include minified bundle in package

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # NPM Package Boilerplate
 
 Simple npm package made for modern browsers.
+
+To use it directly in browsers use:
+
+```html
+<script type="module">
+import package from 'https://unpkg.com/package@version/dist/bundle.min.js'
+</script>
+```
--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,19 @@
 import resolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 
 const extensions = ['.js', '.ts']
 export default [{
   input: 'src/main.ts',
   output: [
     {
-      dir: 'dist',
+      file: 'dist/main.js',
       format: 'esm'
+    },
+    {
+      file: 'dist/bundle.min.js',
+      format: 'esm',
+      plugins: [terser()]
     }
   ],
   external: [/@babel\/runtime/],
